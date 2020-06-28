17851188826b1dca6b4930552fcfcf55595d4ef9
Gaurav Yadav <51997260+gauravmehta13@users.noreply.github.com>
2020-06-28 22:06:45 +0530

    Update mail.py

--- mail.py
+++ mail.py
@@ -2,7 +2,7 @@ import smtplib
 email = '269mehta@gmail.com' # Your email
 password = '************' # Your email account password
 send_to_email = '270mehta@gmail.com' # Who you are sending the message to
-message1 = 'Container Started Successfully'
+message1 = 'Website is not running due to some error'
 server = smtplib.SMTP('smtp.gmail.com', 587) # Connect to the server
 server.starttls() # Use TLS
 server.login(email, password) # Login to the email server
